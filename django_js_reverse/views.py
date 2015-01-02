b0c58229a22b5f86f397d2939dcb68f374d34af7
zero13cool <zero13cool@yandex.ru>
2015-01-02 15:54:43 +0300

    add JS_EXCLUDE_URLNAMES param

--- django_js_reverse/views.py
+++ django_js_reverse/views.py
@@ -1,11 +1,11 @@
-# -*- coding: utf-8 -*-
+# coding: utf-8
 import re
 import sys
 from itertools import chain
 from django.http import HttpResponse
 
 if sys.version < '3':
-    text_type = unicode
+    text_type = basestring
 else:
     text_type = str
 
@@ -16,12 +16,20 @@ from django.conf import settings
 from django import get_version
 
 from slimit import minify
-from . js_reverse_settings import JS_VAR_NAME, JS_MINIFY, JS_EXCLUDE_NAMESPACES
+from .js_reverse_settings import (JS_VAR_NAME, JS_MINIFY,
+                                  JS_EXCLUDE_NAMESPACES, JS_EXCLUDE_URLNAMES)
 
 
 content_type_keyword_name = 'content_type'
 if get_version() < '1.5':
     content_type_keyword_name = 'mimetype'
+
+
+JS_EXCLUDE_NAMESPACES = getattr(settings, 'JS_REVERSE_EXCLUDE_NAMESPACES',
+                                JS_EXCLUDE_NAMESPACES)
+
+JS_EXCLUDE_URLNAMES = getattr(settings, 'JS_REVERSE_EXCLUDE_URLNAMES',
+                              JS_EXCLUDE_URLNAMES)
 
 
 def urls_js(request=None):
@@ -37,12 +45,9 @@ def urls_js(request=None):
 
     default_urlresolver = urlresolvers.get_resolver(getattr(request, 'urlconf', None))
 
-    # prepare data for namespeced urls
-    exclude_ns = getattr(settings, 'JS_REVERSE_EXCLUDE_NAMESPACES', JS_EXCLUDE_NAMESPACES)
     named_urlresolves = [
         (n_urlresolver, namespace_path, namespace + ':')
         for namespace, (namespace_path, n_urlresolver) in default_urlresolver.namespace_dict.items()
-        if namespace not in exclude_ns
     ]
     url_lists = [prepare_url_list(*args) for args in named_urlresolves]
 
@@ -70,8 +75,14 @@ def prepare_url_list(urlresolver, namespace_path='', namespace=''):
     """
     returns list of tuples [(<url_name>, <namespace_path>, <url_patern_tuple> ), ...]
     """
-    prepared_list = []
     for url_name, url_pattern in urlresolver.reverse_dict.items():
-        if isinstance(url_name, text_type) or isinstance(url_name, str):
-            prepared_list.append([namespace + url_name, namespace_path, url_pattern[0][0]])
-    return prepared_list
+        if not isinstance(url_name, text_type):
+            continue
+
+        if JS_EXCLUDE_NAMESPACES and namespace[:-1] in JS_EXCLUDE_NAMESPACES:
+            continue
+
+        if JS_EXCLUDE_URLNAMES and url_name in JS_EXCLUDE_URLNAMES:
+            continue
+
+        yield [namespace + url_name, namespace_path, url_pattern[0][0]]
